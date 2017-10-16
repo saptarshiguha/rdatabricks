e35b2b344dcc8a1b0856cf44dc7844f758838754
SAPTARSHI GUHA <saptarshi.guha@gmail.com>
2017-10-15 22:35:32 -0700

    First Commit

---
+++ R/dbxClusterAdmin.R
@@ -0,0 +1,194 @@
+library(rjson)
+library(httr)
+ul <- function(s,f,h=as.character) h(unlist(lapply(s,f)))
+
+##' Get the different versions of spark available
+##' @param token obtained from databricks
+##' @param instance from frank
+##' @details see https://docs.databricks.com/api/latest/clusters.html#spark-versions
+##' @export
+dbxSparkVersions<-
+    function(token = options("databricks")[[1]]$token
+            ,instance = options("databricks")[[1]]$instance)
+{
+
+    if(is.null(token) || is.null(instance)) stop("Must provide a token and instance")
+    url <- infuse("https://{{instance}}.cloud.databricks.com/api/2.0/clusters/spark-versions",instance=instance)
+    res <- GET(url, add_headers(Authorization= infuse("Bearer {{token}}",token=token)))
+    res <- fromJSON(content(res,'text'))
+    if(!is.null(res$error_code)){
+        stop(infuse("dbx: {{code}}:{{msg}}",code=res$error_code,msg=res$message))
+    }else {
+        defaultVersion <- uu$default_version_key
+        f <- data.table(keys = unlist(lapply(uu$versions,"[[","key")),
+                        name = unlist(lapply(uu$versions,"[[","name")))
+        f[order(keys),]
+        f[, default:=FALSE]
+        list(default = defaultVersion, versions=f)
+    }
+}
+
+##' Get the different zones
+##' @param token obtained from databricks
+##' @param instance from frank
+##' @details see https://docs.databricks.com/api/latest/clusters.html#list-zones
+##' @return a list of default zone and data table of zones
+##' @export
+dbxZones<-
+    function(token = options("databricks")[[1]]$token
+            ,instance = options("databricks")[[1]]$instance)
+{
+
+    if(is.null(token) || is.null(instance)) stop("Must provide a token and instance")
+    url <- infuse("https://{{instance}}.cloud.databricks.com/api/2.0/clusters/list-zones",instance=instance)
+    res <- GET(url, add_headers(Authorization= infuse("Bearer {{token}}",token=token)))
+    res <- fromJSON(content(res,'text'))
+    if(!is.null(res$error_code)){
+        stop(infuse("dbx: {{code}}:{{msg}}",code=res$error_code,msg=res$message))
+    }else {
+        f <- data.table(zones= res$zones)
+        list(default = res$default_zone, zones=f)
+    }
+}
+
+##' Get the different node types
+##' @param token obtained from databricks
+##' @param instance from frank
+##' @details seehttps://docs.databricks.com/api/latest/clusters.html#list-node-types
+##' @return a list with default node type and data table of nodes
+##' @export
+dbxNodeTypes<-
+    function(token = options("databricks")[[1]]$token
+            ,instance = options("databricks")[[1]]$instance)
+{
+
+    if(is.null(token) || is.null(instance)) stop("Must provide a token and instance")
+    url <- infuse("https://{{instance}}.cloud.databricks.com/api/2.0/clusters/list-node-types",instance=instance)
+    res <- GET(url, add_headers(Authorization= infuse("Bearer {{token}}",token=token)))
+    res <- fromJSON(content(res,'text'))
+    if(!is.null(res$error_code)){
+        stop(infuse("dbx: {{code}}:{{msg}}",code=res$error_code,msg=res$message))
+    }else {
+        ## see https://docs.databricks.com/api/latest/clusters.html#clusternodetype
+        x <- res$node_types
+        f <- data.table(
+            node_type_id = ul(x,function(k) k$node_type_id),
+            memory_mb = ul(x,function(k) k$memory_mb,as.numeric),
+            num_cores = ul(x,function(k) k$num_cores,as.numeric),
+            description = ul(x,function(k) k$description),
+            instance_type_id = ul(x,function(k) k$instance_type_id),
+            is_deprecated = ul(x,function(k) k$is_deprecated,as.logical)
+        )
+        setkey(f,"node_type_id")
+        return(list(default = res$default_node_type_id, nodes =f))
+    }
+}
+
+##' Delete a cluster
+##' @param cluster_id a string
+##' @details see https://docs.databricks.com/api/latest/clusters.html#delete
+##' @return nothing
+##' @export
+dbxDelete <- function(cluster_id
+                    , token = options("databricks")[[1]]$token
+                    , instance = options("databricks")[[1]]$instance)
+{
+
+
+    if(is.null(token) || is.null(instance)) stop("Must provide a token and instance")
+    url <- infuse("https://{{instance}}.cloud.databricks.com/api/2.0/clusters/delete",instance=instance)
+    body <- list("cluster_id" = as.character(cluster_id))
+    
+    res <- POST(url, add_headers(Authorization= infuse("Bearer {{token}}",token=token)),
+                body = body,
+              , encode = "json")
+    TRUE
+}
+
+
+##' Restart a cluster
+##' @param cluster_id a string
+##' @details see https://docs.databricks.com/api/latest/clusters.html#restart
+##' @return nothing
+##' @export
+dbxRestart <- function(cluster_id
+                    , token = options("databricks")[[1]]$token
+                    , instance = options("databricks")[[1]]$instance)
+{
+
+
+    if(is.null(token) || is.null(instance)) stop("Must provide a token and instance")
+    url <- infuse("https://{{instance}}.cloud.databricks.com/api/2.0/clusters/restart",instance=instance)
+    body <- list("cluster_id" = as.character(cluster_id))
+    res <- POST(url, add_headers(Authorization= infuse("Bearer {{token}}",token=token)),
+                body = body,
+              , encode = "json")
+    TRUE
+}
+
+
+##' Starts a terminated Spark cluster given its id
+##' @param cluster_id a string
+##' @details see https://docs.databricks.com/api/latest/clusters.html#restart
+##' @return nothing
+##' @export
+dbxStart <- function(cluster_id
+                    , token = options("databricks")[[1]]$token
+                    , instance = options("databricks")[[1]]$instance)
+{
+
+
+    if(is.null(token) || is.null(instance)) stop("Must provide a token and instance")
+    url <- infuse("https://{{instance}}.cloud.databricks.com/api/2.0/clusters/start",instance=instance)
+    body <- list("cluster_id" = as.character(cluster_id))
+    res <- POST(url, add_headers(Authorization= infuse("Bearer {{token}}",token=token)),
+                body = body,
+              , encode = "json")
+    TRUE
+}
+
+##' Returns an autoscale structure
+##' @param  min_workers The minimum number of workers to which the cluster can scale down when underutilized. It is also the initial number of workers the cluster will have after creation.
+##' @param max_workers The maximum number of workers 
+##' @details see https://docs.databricks.com/api/latest/clusters.html#clusterautoscale
+##' @return an object of class autoscale
+##' @export
+autoscale <- function(min_workers, max_workers)
+{
+    x <- list(min_workers=as.integer(min_workers),
+              max_workers=as.integer(max_workers))
+    if(max_workers < min_workers) stop("max must be larger than min")
+    class(x) = "autoscale"
+    return(x)
+}
+
+
+
+##' Resize a Spark cluster given its id and num_workers
+##' @param cluster_id a string
+##' @param num_workers an integer or of class autoscale
+##' @details see https://docs.databricks.com/api/latest/clusters.html#resize
+##' @return nothing
+##' @export
+dbxResize <- function(cluster_id,
+                    , token = options("databricks")[[1]]$token
+                    , instance = options("databricks")[[1]]$instance)
+{
+
+
+    if(is.null(token) || is.null(instance)) stop("Must provide a token and instance")
+    url <- infuse("https://{{instance}}.cloud.databricks.com/api/2.0/clusters/resize",instance=instance)
+    if(!(is(num_workers,"integer") || is(num_workers,"autoscale"))){
+        stop("num_workers must be an integer or of class autoscale")
+    }
+    body <- list("cluster_id" = as.character(cluster_id))
+    if(is(num_workers,"integer")){
+        body$num_workers = num_workers
+    }else{
+        body$autoscale <- num_workers
+    }
+    res <- POST(url, add_headers(Authorization= infuse("Bearer {{token}}",token=token)),
+                body = body,
+              , encode = "json")
+    TRUE
+}
---
+++ R/languageContext.R
@@ -0,0 +1,171 @@
+library(httr)
+library(infuser)
+
+checkOptions <- function(...){
+  parms <- list(...)
+  if(any(unlist(lapply(parms, function(k) is.null(k) || length(k)==0))))
+    stop("One or more passed parameters is NULL or of length zero")
+}
+
+##' Create a Remote Language Context
+##' @param instance is the instance of databricks 
+##' @param clusterId is the clusterId you're working with
+##' @param user your usename
+##' @param password your password
+##' @details see https://docs.databricks.com/api/1.2/index.html#execution-context
+##' @return a context object
+##' @export 
+dbxCtxMake <- function(instance=options("databricks")[[1]]$instance
+                    ,clusterId=options("databricks")[[1]]$clusterId
+                    ,user=options("databricks")[[1]]$user
+                    ,password=options("databricks")[[1]]$password)
+{
+  checkOptions(instance, clusterId,user, password)
+  url <- infuse("https://{{instance}}.cloud.databricks.com/api/1.2/contexts/create",instance=instance)
+  pyctx<-POST(url,body=list(language='python', clusterId=clusterId)
+    ,encode='form'
+    ,authenticate(user,password))
+  pyctxId <- content(pyctx)$id
+}
+  
+##' Get Status of a Context
+##' @param ctx the context for the language
+##' @param instance is the instance of databricks 
+##' @param clusterId is the clusterId you're working with
+##' @param user your usename
+##' @param password your password
+##' @details see https://docs.databricks.com/api/1.2/index.html#execution-context
+##' @return a context object
+##' @export 
+dbxCtxStatus <- function(ctx
+                    ,instance=options("databricks")[[1]]$instance
+                    ,clusterId=options("databricks")[[1]]$clusterId
+                    ,user=options("databricks")[[1]]$user
+                    ,password=options("databricks")[[1]]$password)
+{
+  checkOptions(instance, clusterId,user, password)
+  url <- infuse("https://{{instance}}.cloud.databricks.com/api/1.2/contexts/status",instance=instance)
+  ctxStatusCall<-GET(url,query=list(clusterId=clusterId,contextId=ctx)
+            ,authenticate(user,password))
+  content(ctxStatusCall)
+}
+
+##' Destroy. a Context
+##' @param ctx the context for the language
+##' @param instance is the instance of databricks 
+##' @param clusterId is the clusterId you're working with
+##' @param user your usename
+##' @param password your password
+##' @details see https://docs.databricks.com/api/1.2/index.html#execution-context
+##' @return an id you have nothing to do with
+##' @export 
+dbxCtxDestroy <- function(ctx
+                    ,instance=options("databricks")[[1]]$instance
+                    ,clusterId=options("databricks")[[1]]$clusterId
+                    ,user=options("databricks")[[1]]$user
+                    ,password=options("databricks")[[1]]$password)
+{
+ checkOptions(instance, clusterId,user, password)
+  url <- infuse("https://{{instance}}.cloud.databricks.com/api/1.2/contexts/destroy",instance=instance)
+  ctxDestroyCall<-POST(url,body=list(clusterId=clusterId,contextId=ctx)
+                     ,encode='form'
+                     ,authenticate(user,password))
+  ctxDestroy <- content(ctxDestroyCall)
+}
+
+
+##' Command execute
+##' @param command a string command to be executed
+##' @param wait if non zero, will wait till command is finnished. wait is seconds polling
+##' @param ctx the context for the language
+##' @param instance is the instance of databricks 
+##' @param clusterId is the clusterId you're working with
+##' @param user your usename
+##' @param password your password
+##' @details see https://docs.databricks.com/api/1.2/index.html#command-execution
+##' @return a commandId
+##' @export 
+dbxRunCommand <- function(command, wait=0
+                    ,ctx
+                    ,instance=options("databricks")[[1]]$instance
+                    ,clusterId=options("databricks")[[1]]$clusterId
+                    ,user=options("databricks")[[1]]$user
+                    ,password=options("databricks")[[1]]$password)
+{
+  checkOptions(command,ctx,instance, clusterId,user, password)
+  url <- infuse("https://{{instance}}.cloud.databricks.com/api/1.2/commands/execute"
+  commandUrl<-POST(url
+            ,body=list(language='python'
+                       ,clusterId=clusterId
+                       ,contextId=ctx
+                       ,command=command)
+            ,encode='form'
+            ,authenticate(user,password))
+  commandCtx <- content(commandUrl)$id
+  if(wait>0){
+    while(TRUE){
+      status = dbxCmdStatus(commandCtx,ctx,instance,clusterId,user,password)
+      if(hasEnded(status)) return(status) else sleep(wait)
+    }
+  }else{
+    commandCtx
+  }
+}
+
+##' Command Status
+##' @param cmdId commandId returned by runCommand
+##' @param ctx the context for the language
+##' @param instance is the instance of databricks 
+##' @param clusterId is the clusterId you're working with
+##' @param user your usename
+##' @param password your password
+##' @details see https://docs.databricks.com/api/1.2/index.html#command-execution
+##' @return a commandId
+##' @export 
+dbxCmdStatus <- function(cmdId,
+                    ,ctx
+                    ,instance=options("databricks")[[1]]$instance
+                    ,clusterId=options("databricks")[[1]]$clusterId
+                    ,user=options("databricks")[[1]]$user
+                    ,password=options("databricks")[[1]]$password)
+{
+  checkOptions(cmdId,ctx,instance, clusterId,user, password)
+  url <- infuse("https://{{instance}}.cloud.databricks.com/api/1.2/commands/status",instance=instance)
+  cmdStatusCall<-GET(url,query=list(clusterId=clusterId
+                                  ,contextId=ctx
+                                  ,commandId=cmdId)
+                   ,authenticate(user,password))
+  (cmdStatus <- content(cmdStatusCall))
+  return(cmdStatus)
+}
+
+##' Command Cancel
+##' @param cmdId commandId returned by runCommand
+##' @param ctx the context for the language
+##' @param instance is the instance of databricks 
+##' @param clusterId is the clusterId you're working with
+##' @param user your usename
+##' @param password your password
+##' @details see https://docs.databricks.com/api/1.2/index.html#command-execution
+##' @return a commandId
+##' @export 
+dbxCmdCancel <- function(cmdId,
+                    ,ctx
+                    ,instance=options("databricks")[[1]]$instance
+                    ,clusterId=options("databricks")[[1]]$clusterId
+                    ,user=options("databricks")[[1]]$user
+                    ,password=options("databricks")[[1]]$password)
+{
+  checkOptions(cmdId,ctx,instance, clusterId,user, password)
+  url <- infuse("https://{{instance}}.cloud.databricks.com/api/1.2/commands/cancel"
+              ,instance=instance)
+  cancelUrl<-POST(url
+                 ,body=list(clusterId=clusterId
+                            ,contextId=ctx
+                            ,commandId=cmdId)
+                 ,encode='form'
+                 ,authenticate(user,password))
+  (cancelCtx <- content(cancelUrl))
+  return(cancelCtx)
+}
+
