e6393f533798252c4f8d70db83a7755d1cec414f
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2018-03-01 13:30:29 -0800

    Update dbxClusterAdmin.R

--- R/dbxClusterAdmin.R
+++ R/dbxClusterAdmin.R
@@ -222,8 +222,9 @@ dbxGet <- function(cluster_id
     res <- POST(url, add_headers(Authorization= infuse("Bearer {{token}}",token=token)),
                 body = body
               , encode = "json")
-   r<- tryParsing(res)
-   if(r$status) r$content else stop(r$content)
+    content(res,as='parsed')
+#   r<- tryParsing(res)
+#   if(r$status) r$content else stop(r$content)
 }
 
 
